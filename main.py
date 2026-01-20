1f861dafbb9eb2303103e2c55b65740182330a45
aashuchoudhary128 <aashuchoudhary128@gmail.com>
2026-01-20 22:04:12 +0530

    Update greeting logic

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
-# This is a sample Python script.
+# Lab Assignment 1
+# Employee Salary Program
 
-# Press Ctrl+F5 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+name = input("Enter Employee Name: ")
+emp_id = input("Enter Employee ID: ")
+department = input("Enter Department: ")
+basic_salary = float(input("Enter Basic Salary: "))
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+da = 0.92 * basic_salary
+hra = 0.58 * basic_salary
+ta = 0.30 * basic_salary
+lic = 500
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+gross_salary = basic_salary + da + hra + ta
+net_salary = gross_salary - lic
+
+print("\n----- Employee Salary Details -----")
+print("Name:", name)
+print("Employee ID:", emp_id)
+print("Department:", department)
+print("Basic Salary:", basic_salary)
+print("DA:", da)
+print("HRA:", hra)
+print("TA:", ta)
+print("LIC Deduction:", lic)
+print("Net Salary:", net_salary)
+
