965f6e9260fbe8f64dea01f219138e6459064847
aashuchoudhary128 <aashuchoudhary128@gmail.com>
2026-03-04 23:22:47 +0530

    numpy

--- practical4_LabAssignment2.py
+++ practical4_LabAssignment2.py
@@ -1,16 +1,21 @@
+
 import numpy as np
-print("Enter elements for 5x3 matrix:")
-matrix1 = []
+matrix_a = []
+print("Enter elements for 5x3 matrix (3 numbers per row):")
 for i in range(5):
-    row = list(map(int, input(f"Enter 3 elements for row {i+1} separated by space: ").split()))
-    matrix1.append(row)
-matrix1 = np.array(matrix1)
-print("\nEnter elements for 3x2 matrix:")
-matrix2 = []
+    row = list(map(int, input(f"Row {i+1}: ").split()))
+    matrix_a.append(row)
+matrix_a = np.array(matrix_a)
+matrix_b = []
+print("Enter elements for 3x2 matrix (2 numbers per row):")
 for i in range(3):
-    row = list(map(int, input(f"Enter 2 elements for row {i+1} separated by space: ").split()))
-    matrix2.append(row)
-matrix2 = np.array(matrix2)
-product = np.dot(matrix1, matrix2)
+    row = list(map(int, input(f"Row {i+1}: ").split()))
+    matrix_b.append(row)
+matrix_b = np.array(matrix_b)
+product = np.zeros((5, 2), dtype=int)
+for i in range(5):
+    for j in range(2):
+        for k in range(3):
+            product[i][j] += matrix_a[i][k] * matrix_b[k][j]
 print("\nProduct Matrix (5x2):")
 print(product)
